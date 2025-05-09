c306e5fa1723d35615f29c084c071b0e897afc13
GES <ges@Mac.home>
2025-05-09 21:10:08 +0200

    Testing function environtment

--- functions/index.js
+++ functions/index.js
@@ -7,19 +7,19 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const functions = require('firebase-functions');
+// const functions = require('firebase-functions');
 
-const firebaseConfig = {
-  apiKey: functions.config().config.api_key,
-  authDomain: functions.config().config.auth_domain,
-  projectId: functions.config().config.project_id,
-  storageBucket: functions.config().config.storage_bucket,
-  messagingSenderId: functions.config().config.messaging_sender_id,
-  appId: functions.config().config.app_id,
-  measurementId: functions.config().config.measurement_id,
-};
+// const firebaseConfig = {
+//   apiKey: functions.config().config.api_key,
+//   authDomain: functions.config().config.auth_domain,
+//   projectId: functions.config().config.project_id,
+//   storageBucket: functions.config().config.storage_bucket,
+//   messagingSenderId: functions.config().config.messaging_sender_id,
+//   appId: functions.config().config.app_id,
+//   measurementId: functions.config().config.measurement_id,
+// };
 
-console.log('Firebase Config:', firebaseConfig);
+// console.log('Firebase Config:', firebaseConfig);
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
