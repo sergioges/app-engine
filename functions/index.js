6fb7929d9b59f565b34d407dda6af284d30a5ad6
GES <ges@Mac.home>
2025-05-09 20:00:51 +0200

    Add logic for firebase database

---
+++ functions/index.js
@@ -0,0 +1,33 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+const functions = require('firebase-functions');
+
+const firebaseConfig = {
+  apiKey: functions.config().config.api_key,
+  authDomain: functions.config().config.auth_domain,
+  projectId: functions.config().config.project_id,
+  storageBucket: functions.config().config.storage_bucket,
+  messagingSenderId: functions.config().config.messaging_sender_id,
+  appId: functions.config().config.app_id,
+  measurementId: functions.config().config.measurement_id,
+};
+
+console.log('Firebase Config:', firebaseConfig);
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
