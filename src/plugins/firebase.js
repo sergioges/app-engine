87e33993545b61ca413134a27d2e3db29b29a455
GES <ges@Mac.home>
2025-05-09 21:23:57 +0200

    Update firebase variables

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -10,13 +10,13 @@ import { getAuth } from 'firebase/auth';
 
 // Configuración de Firebase (reemplaza con tus credenciales)
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    apiKey: import.meta.env.VITE_FBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FBASE_MEASUREMENT_ID,
 };
 
 // Inicializa Firebase
