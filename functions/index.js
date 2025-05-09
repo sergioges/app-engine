943adcae99f2457c7b36ab4e2df966ae88abb14e
GES <ges@Mac.home>
2025-05-09 20:47:43 +0200

    Update firebase functions

--- functions/index.js
+++ functions/index.js
@@ -7,9 +7,6 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
 const functions = require('firebase-functions');
 
 const firebaseConfig = {
@@ -27,7 +24,5 @@ console.log('Firebase Config:', firebaseConfig);
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// firebase functions:config:get --> Get API keys
+// firebase functions:config:set config
